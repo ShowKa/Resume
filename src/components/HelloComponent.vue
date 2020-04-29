89d8a1b76c5062627b0c7d44c610f193c7e78e50
John Doe <johndoe@example.com>
2020-04-29 22:55:56 +0900

    Properties File

--- src/components/HelloComponent.vue
+++ src/components/HelloComponent.vue
@@ -1,14 +1,8 @@
 <template>
-  <h1>Hello {{ name }}!</h1>
+  <h1>Hello {{ $name }}!</h1>
 </template>
 <script>
 export default {
-  props: {
-    name: {
-      type: String,
-      required: true
-    }
-  }
 }
 </script>
 <style lang="scss" scoped>
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Properties from './Properties.js'
 import '../assets/app.scss'
 // BootstrapのJavaScript側の機能
 import 'bootstrap'
 // スタイルシート
 import './index.scss'
+
+// Properties
+Object.assign(Vue.prototype, Properties)
 
 /* eslint-disable-next-line no-new */
 new Vue({
