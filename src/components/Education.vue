0c8f2d82a79e2e7578b6db14f894624b3cf162a0
John Doe <johndoe@example.com>
2020-04-30 22:04:06 +0900

    Education: School history

--- src/components/Education.vue
+++ src/components/Education.vue
@@ -1,11 +1,42 @@
 <template>
-  <div class="education" />
+  <div class="education">
+    <div class="education__title">
+      学習歴
+    </div>
+    <div class="education__ul">
+      <School
+        v-for="(school, i) in $SchoolList"
+        :key="'school_' + i"
+        class="education__li"
+        :name="school.name"
+        :major="school.major"
+        :entry="school.entry"
+        :graduate="school.graduate"
+      />
+    </div>
+  </div>
 </template>
 
 <script>
-export default {}
+import School from './School.vue'
+export default {
+  components: {
+    School
+  }
+}
 </script>
 
 <style lang="scss">
 @import 'assets/app.scss';
+.education {
+  &__title {
+    @extend %text_chapter-title;
+  }
+  &__ul {
+    margin-top: 30px;
+  }
+  &__li {
+    margin-top: 15px;
+  }
+}
 </style>
---
+++ src/components/School.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="school">
+    <div class="school__name">
+      <span>
+        {{ name }}
+      </span>
+      <span>,</span>
+      <span class="school__major">
+        {{ major }}
+      </span>
+    </div>
+    <div class="school__date">
+      <span> {{ entry }} - {{ graduate }} </span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+    major: {
+      type: String,
+      required: true
+    },
+    entry: {
+      type: String,
+      required: true
+    },
+    graduate: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import 'assets/app.scss';
+.school {
+  &__name {
+    @extend %text_short;
+  }
+  &__major {
+    color: $color_primary_variant;
+  }
+  &__date {
+    @extend %text_short_light;
+  }
+}
+</style>
