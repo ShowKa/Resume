56e9715b64cdb881a1f8d579ec0d7af33787f9d1
John Doe <johndoe@example.com>
2020-04-30 09:39:21 +0900

    remove HeaderComponent

--- src/components/Field.vue
+++ src/components/Field.vue
@@ -1,10 +1,5 @@
 <template>
 <div class="field container">
-  <div class="field__header row">
-    <div class="col-md-12">
-      <HeaderComponent />
-    </div>
-  </div>
   <div class="field__body row">
     <div class="col">
       <BodyComponent />
@@ -13,12 +8,10 @@
 </div>
 </template>
 <script>
-import HeaderComponent from './HeaderComponent.vue'
 import BodyComponent from './BodyComponent.vue'
 export default {
   components: {
-    BodyComponent,
-    HeaderComponent
+    BodyComponent
   },
   methods: {}
 }
@@ -26,19 +19,11 @@ export default {
 <style lang="scss">
 @import 'assets/app.scss';
 .field {
-    // var
-    $header_ratio: 0.2;
     // style
     height: $width * 1.5;
     border: 1px solid #aaaaaa;
-    &__header {
-      height: percentage($header_ratio);
-      padding-top: 30px;
-      padding-bottom: 30px;
-      padding-left: 30px;
-    }
     &__body {
-      height: percentage(1 - $header_ratio);
+      height: 100%;
       padding-top: 30px;
       padding-bottom: 30px;
       padding-left: 30px;
